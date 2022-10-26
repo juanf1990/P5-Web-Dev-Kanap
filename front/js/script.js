bc51c87965a36d4e144309bd40aa8fd390e7874f
Juan <juanandres15@gmail.com>
2022-10-26 15:50:56 +0100

    added js files

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+
+/**
+ * Display message in console
+ * 
+ * @param {string} message - The message to display 
+ */
+function display(message) {
+  console.log(message);
+}
+display('This is a test');
